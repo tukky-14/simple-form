77c6f828a32b205893be61521e5b4b3804472a91
Eiji Otsuki <bigoonage0104@gmail.com>
2020-09-08 19:17:51 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  templateUrl: './simple-form.component.html',
+  styleUrls: ['./simple-form.component.css']
+})
+export class SimpleFormComponent implements OnInit {
+  text1: string;
+  text2: string;
+  result: string = "足し算しましょう";
+
+  addAndShow(): void {
+    let forResult: string="正しい値を入力してください";
+    let int1: number;
+    let int2: number;
+
+    int1 = Number(this.text1);
+    int2 = Number(this.text2);
+
+    // int1とint2が数値以外でなかったなら
+    if(!Number.isNaN(int1) && !Number.isNaN(int2)) {
+      forResult = `${int1}+${int2}=${int1+int2}`;
+    }
+
+    this.result=forResult;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
