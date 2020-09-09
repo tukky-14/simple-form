dfb20b58d634aa4023ae6c5e750efc7f3891fa07
Eiji Otsuki <bigoonage0104@gmail.com>
2020-09-09 20:25:21 +0900

    Complete simple-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// 利用したいコンポーネントをインポートして参照できるようにする
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { BetterFormComponent } from './better-form/better-form.component';
+import { ControlsComponent } from './controls/controls.component';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // URLパスとモジュールをforRootメソッドで関連づける
+  imports: [RouterModule.forRoot([
+    {path:'', redirectTo: '/simple-form', pathMatch: 'full'},
+    {path: 'simple-form', component:SimpleFormComponent },
+    {path: 'better-form', component:BetterFormComponent },
+    {path: 'controls', component:ControlsComponent }
+  ]
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
